a1a871ead5bfd6ffe9ff20d981bddf689ae673c6
maiaboelmagde <maishalan00000@gmail.com>
2023-09-11 13:59:04 -0700

    uploading 2nd task solution

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - print all alphabets in small.
  * Return: 0 (Success)
- */
+*/
+
 int main(void)
 {
-	int n, m;
+	char c = 'a';
 
-	for (n = 48; n <= 56; n++)
+	while (c <= 'z')
 	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+		putchar(c);
+		c++;
 	}
 	putchar('\n');
+
 	return (0);
 }
