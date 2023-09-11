bd537ae5479dd1dab25ef55b8cf73b4890839a96
maiaboelmagde <maishalan00000@gmail.com>
2023-09-11 14:09:05 -0700

    uploading 5th task solution

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main -  printing all single digit numbers of base 10
+ * Return:  0
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		printf("%i", d);
+		d++;
+	}
+	printf("\n");
+	return (0);
+}
