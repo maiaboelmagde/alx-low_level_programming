bdb0fe53a107d4a9b6ebbafe0b8e0a92a3660799
maiaboelmagde <maishalan00000@gmail.com>
2023-09-11 12:56:23 -0700

    uploading 4th task solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ *C program that prints exactly ("Programming is like building a multilingual puzzle) using puts
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
