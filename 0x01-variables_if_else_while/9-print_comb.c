f5bf825bc6ec31a2527918a0ddd9fc1137619921
maiaboelmagde <maishalan00000@gmail.com>
2023-09-11 14:18:08 -0700

    uploading 9th task solution

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  printing all possible combinations of single-digit numbers
+ * Return: 0
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d + 48);
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
