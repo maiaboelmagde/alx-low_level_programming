982470f29704be9d1d269a39941b58f18b75f191
mai <maishalan00000@gmail.com>
2023-10-03 17:29:42 +0300

    uploading 1st task solotion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_print_rev_recursion - 1st task function
+*@s: the string that would be reversed
+**/
+void _print_rev_recursion(char *s){
+if (*s){
+_print_rev_recursion(s+1);
+_putchar(*s);
+}
+else
+_putchar('/n');
+}
