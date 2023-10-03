842ba2782572348e7b5e6650ca0deea23ee5fbf8
mai <maishalan00000@gmail.com>
2023-10-03 16:48:01 +0300

    uploading 0th task solotion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,14 @@
 *@s: input_iterator_tag
 *Return: Always 0 (success)
 */
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
 if (*s)
 {
-_putchar (*s);
-_puts_recursion (s + 1);
+_putchar(*s);
+_puts_recursion(s + 1);
 }
 else
-_putchar ('\n');
+_putchar('\n');
 
 }
