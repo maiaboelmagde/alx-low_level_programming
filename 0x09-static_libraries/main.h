5db8f1281e73565eba9dbb806cfa99212da83ae3
maiaboelmagde <maishalan00000@gmail.com>
2023-10-07 18:38:45 -0700

    uploading 0th task solution

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -5,10 +5,14 @@
 #include <string.h>
 #include <unistd.h>
 
-int _putchar(char c); int _islower(int c);
-int _isalpha(int c); int _abs(int n);
-int _isupper(int c); int _isdigit(int c);
-int _strlen(char *s); void _puts(char *s);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
