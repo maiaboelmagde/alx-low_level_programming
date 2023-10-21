299e94bda4597a7b2a091dc1c42e98cf01ee841b
mai <maishalan00000@gmail.com>
2023-10-21 03:59:30 +0300

    uploading the 5th task sokution file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void pre_main(void) __attribute__ ((constructor));
 
 /**
- * first - print text before main
+ * pre_main -  function should be executed before main function 
+ *
+ * Return: void.
  */
-void first(void)
+void pre_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
