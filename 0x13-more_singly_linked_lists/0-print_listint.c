bee5afba1fac2426c89cfe7ebc600ebe935eea8a
mai <maishalan00000@gmail.com>
2023-10-24 11:15:37 +0300

    uploading the 0th task solution

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+*print_listint - a function that prints all the elements of a listint_t list.
+*@h : lisr_t list
+**/
+
+size_t print_listint(const listint_t *h)
+{
+
+    int ret=0;
+
+    while (h!= NULL){
+        printf("%u\n",h->n);
+        h=h->next;
+        ret++;
+    }
+
+    return ret;
+
+}
