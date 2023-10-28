cff50d98c7f2b63e8900a70fc6b04026098d43c5
mai <maishalan00000@gmail.com>
2023-10-29 00:58:27 +0200

    uploading 0th task solution

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * calc_len - a function to calculate the lenth of the string
+ * @s: the string
+ * Return: the length
+*/
+int calc_len(const char* s){
+	int count=0;
+	while (s[count] != '\0'){
+		count++;
+	}
+	return count;
+}
+
+/**
+ * binary_to_uint - a function that converts a binary number to an unsigned int.
+ * 
+ * @b: the binary pattern
+ * Return : decimal equavellant
+*/
+
+unsigned int binary_to_uint(const char *b){
+	unsigned int sum;
+	int counter , i , n;
+	sum = 0;
+	counter = 1;
+	if (b== NULL)
+		return 0 ;
+	n = calc_len(b);
+	for (i = n-1 ; i >= 0 ; i--){
+		if (b[i] == '1' ){
+			sum += counter;
+		}else if (b[i] != '0')
+			return 0;
+		counter *= 2;
+	}
+	return sum;
+}
