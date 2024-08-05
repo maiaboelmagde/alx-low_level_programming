76e38b7062d6fbb806be24974b7f035622aea9c1
maiaboelmagd <maishalan00000@gmail.com>
2024-08-06 02:57:00 +0300

    fixing betty of the 4th task solution

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,20 +8,19 @@
 
 int _pow_recursion(int x, int y)
 {
-    if (y == 1)
-    {
-        return (x);
-    }else if (y < 0)
-    {
-        return -1;
-    }else if (y == 0)
-    {
-        return (1);
-    }else if (y % 2)
-    {
-        return (x * _pow_recursion(x, y - 1));
-    }else
-    {
-        return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
-    }
+	if (y == 1)
+	{
+		return (x);
+	} else if (y < 0)
+	{
+		return (-1);
+	} else if (y == 0)
+	{
+		return (1);
+	} else if (y % 2)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+
 }
